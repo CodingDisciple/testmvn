4502d8edc3ba9e60a6b1e833888e3b523b3d29ba
antiecheng <antiecheng@meituan.com>
2018-07-25 13:43:04 +0800

    添加jodatime测试类

---
+++ src/main/java/com/mt/test/guava/JodaTimeTest.java
@@ -0,0 +1,14 @@
+package com.mt.test.guava;
+
+import org.joda.time.DateTime;
+
+/**
+ * Created by antiecheng on 2018/7/25.
+ */
+public class JodaTimeTest {
+
+  public static void main(String[] args) {
+      DateTime time = new DateTime();
+    System.out.println(time.toString());
+   }
+}
